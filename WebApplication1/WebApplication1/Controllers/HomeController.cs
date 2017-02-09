00971bb26e5f6608b221cb35166b6fcbb2df4148
bpszarol <bpszarol@mtu.edu>
2017-02-08 21:20:47 -0500

    Added more projects

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -29,13 +29,31 @@ namespace WebApplication1.Controllers
 
         public ActionResult Project1()
         {
-            ViewBag.Message = "This is a basic introduction to knockout";
+            ViewBag.Message = "This is a simple introduction to using knockout using input fields and ko.computed functions to check for input and modify existing text elements";
             return View();
         }
 
         public ActionResult Project2()
         {
-            ViewBag.Message = "Second Project";
+            ViewBag.Message = "This is a simple introduction to other method of data input, as well as using ko.computed functions to perform mathematical operations on input";
+            return View();
+        }
+
+        public ActionResult Project3()
+        {
+            ViewBag.Message = "This is a simple introduction to observable arrays that can be bound by specific rules.";
+            return View();
+        }
+
+        public ActionResult Project4()
+        {
+            ViewBag.Message = "This is an introduction to using JSON and YQL to get current news headlines and dislpay them in a scrollable box.";
+            return View();
+        }
+
+        public ActionResult Project5()
+        {
+            ViewBag.Message = "This is SPARTA.";
             return View();
         }
     }
